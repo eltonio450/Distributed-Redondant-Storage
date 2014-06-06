ecc1b04ac495ae43facadb8f18c77f3967eca44c
Eltonio <antoine.sauvage@melix.net>
2014-06-06 16:45:53 +0200

    Mise en place de la lecture ligne de commande

---
+++ src/ServerDebug.java
@@ -0,0 +1,28 @@
+
+public class ServerDebug extends Thread {
+	
+	int identifiantDebug;
+	int port;
+	String [] args;
+	
+	
+	public ServerDebug(int id, int port)
+	{
+		args = new String[5];
+		args[0]="";
+		args[1]="";
+		args[2]="-d";
+		args[3]=Integer.valueOf(identifiantDebug).toString();
+		args[4]="-p";
+		args[5]=Integer.valueOf(port).toString();
+		
+	}
+	
+	
+	
+	
+	
+	public void run(String[] args){
+		Main.main(args);		
+	}
+}
--- src/Test.java
+++ src/Test.java
@@ -7,11 +7,13 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		int [] listeServeur = new int [50]; 
 		String[] arguments = new String[1];
 		arguments[0] = "5035";
 		
 		System.out.println("Test");
+		
+		Main serveur = new 
 		//Seb : coucou !! 
 		//main(arguments);
 		//
