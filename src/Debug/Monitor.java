c918f1028987834a7982d6a9d0fe0592db034854
Eltonio <antoine.sauvage@melix.net>
2014-06-13 17:32:45 +0200

    Creation Monitor

---
+++ src/Debug/Monitor.java
@@ -0,0 +1,18 @@
+package Debug;
+
+import java.nio.channels.SocketChannel;
+
+import Utilitaires.*;
+
+public class Monitor extends Thread{
+	
+	SocketChannel socket;
+	
+	public Monitor(SocketChannel socket){
+		
+	}
+	
+	public void run(){
+		
+	}
+}
