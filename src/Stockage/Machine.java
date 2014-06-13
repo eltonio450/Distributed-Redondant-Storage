5c80a42e247d378223797463bcf5620ff04b6fce
Simon Matet <simon.matet@polytechnique.edu>
2014-06-13 14:16:41 +0200

    azerty

--- src/Stockage/Machine.java
+++ src/Stockage/Machine.java
@@ -3,9 +3,11 @@ package Stockage;
 public class Machine {
 
   String IpAdresse ;
+  int port;
   
-  Machine(String addr){
+  Machine(String addr, int port){
     IpAdresse = addr ;
+    this.port = port;
   }
   
 }
