6a3e9ff5c71666cb665af2805aa9e54bde33d2b9
sds-dubois <sdubois.sebastien@gmail.com>
2014-06-13 14:40:58 +0200

    Seb Vendredi 13/06 14h40

--- src/Stockage/SendPaquet.java
+++ src/Stockage/SendPaquet.java
@@ -4,11 +4,25 @@ import java.util.ArrayList;
 
 public class SendPaquet {
 
-  public static void prevenirHostChanged(Machine m,Long idPaquet) {
-    
+  public static void envoieData(Donnees d,ArrayList<Paquet> listPaquets, ArrayList<Machine> hosts){
+    //envoie un groupe de paquets à un groupe d'hosts
+    //stock les paquets reçus en échange dans d
+    int n = listPaquets.size() ; //should equal 5
+    assert n == hosts.size() ;
+    for (int j = 0 ; j< n ; j++){
+      Paquet p = listPaquets.get(j) ;
+      p.putOtherHosts(hosts);
+      Paquet q = putAndGet(hosts.get(j),p) ;
+      d.addHost(hosts.get(j)) ;
+    }
   }
   
-  public static void envoieData(Donnees data, ArrayList<Paquet> listPaquets, ArrayList<Machine> hosts) {
-    
+  public static Paquet putAndGet(Machine m, Paquet p){ //TODO
+    //Envoie UN paquet à m et reçoit UN paquet en échange
+    return null ;
+  }
+  
+  public static void prevenirHostChanged(Machine m , long Id){  //TODO
+    //préviens une machine que cette machine remplace m pour le paquet d'id Id
   }
 }
