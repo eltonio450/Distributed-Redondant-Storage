ffa9520c9c66af5c1ca2d4180898f1b3bb39eb70
Simon Matet <simon.matet@polytechnique.edu>
2014-06-13 15:36:10 +0200

    ServeurTCP fini :)

--- src/TCPConnections/GeneralPurposeRequestAnalyzer.java
+++ src/TCPConnections/GeneralPurposeRequestAnalyzer.java
@@ -1,5 +1,16 @@
 package TCPConnections;
 
+import java.nio.channels.SocketChannel;
+import java.util.LinkedList;
+
 public class GeneralPurposeRequestAnalyzer extends Thread {
-	//LinkedList<Requester> aTraiter;
+	LinkedList<SocketChannel> aTraiter;
+	
+	public void run () {
+		
+	}
+	
+	public void addRequester(SocketChannel requester) {
+		
+	}
 }
