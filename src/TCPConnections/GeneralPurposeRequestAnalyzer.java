c51302af2e3a81e2312d882bb24f9fb671eee77a
sds-dubois <sdubois.sebastien@gmail.com>
2014-06-13 16:28:34 +0200

    Seb 16h28

--- src/TCPConnections/GeneralPurposeRequestAnalyzer.java
+++ src/TCPConnections/GeneralPurposeRequestAnalyzer.java
@@ -2,15 +2,38 @@ package TCPConnections;
 
 import java.nio.channels.SocketChannel;
 import java.util.LinkedList;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class GeneralPurposeRequestAnalyzer extends Thread {
 	LinkedList<SocketChannel> aTraiter;
+	LinkedList<SocketChannel> aAjouter;
+	LinkedList<SocketChannel> aEnlever;
+	ReentrantLock lock;
 	
 	public void run () {
+		aTraiter = new LinkedList<SocketChannel> ();
+		aAjouter = new LinkedList<SocketChannel> ();
+		aEnlever = new LinkedList<SocketChannel> ();
+		lock = new ReentrantLock ();
 		
+		while (true) {
+			for (SocketChannel s : aTraiter) {
+				// Traitement
+			}
+			
+			lock.lock();
+			aTraiter.addAll(aAjouter);
+			aTraiter.clear();
+			lock.unlock();
+			for (SocketChannel s : aEnlever) {
+				aTraiter.remove(s);
+			}
+		}
 	}
 	
 	public void addRequester(SocketChannel requester) {
-		
+		lock.lock();
+		aAjouter.add(requester);
+		lock.unlock();
 	}
 }
