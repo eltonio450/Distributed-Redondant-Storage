9d94bb058a88187df63945151af7bf95074b10b0
sds-dubois <sdubois.sebastien@gmail.com>
2014-06-13 14:30:30 +0200

    Seb Vendredi 13/06 14h30

---
+++ src/Stockage/SendPaquet.java
@@ -0,0 +1,14 @@
+package Stockage;
+
+import java.util.ArrayList;
+
+public class SendPaquet {
+
+  public static void prevenirHostChanged(Machine m,Long idPaquet) {
+    
+  }
+  
+  public static void envoieData(Donnees data, ArrayList<Paquet> listPaquets, ArrayList<Machine> hosts) {
+    
+  }
+}
