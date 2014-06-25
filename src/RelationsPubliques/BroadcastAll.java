9ae416a529620c89bd4a42980ef0386c171e7f33
Simon Matet <simon.matet@polytechnique.edu>
2014-06-25 20:39:31 +0200

    Broadcast

--- src/RelationsPubliques/BroadcastAll.java
+++ src/RelationsPubliques/BroadcastAll.java
@@ -12,21 +12,20 @@ public class BroadcastAll {
 	public static void broadcastAllUDP (String message) {
 		// Not useful
 	}
-	
+
 	public static void broadcastTCP (String message, LinkedList<Machine> liste) {
 		ByteBuffer b = Utilitaires.Utilitaires.stringToBuffer(message);
-		try (SocketChannel clientSocket = SocketChannel.open()) { 
-			for (Machine m : liste) {
-				try {
-					InetSocketAddress local = new InetSocketAddress(0); 
-					clientSocket.bind(local); 
-					InetSocketAddress remote = new InetSocketAddress(m.ipAdresse, m.port); 
-					clientSocket.connect(remote); 
-					clientSocket.write(b);
-				} catch (Exception e) {
-					//Who cares ?
-				}
+		for (Machine m : liste) {
+			try (SocketChannel clientSocket = SocketChannel.open()) { 
+				InetSocketAddress local = new InetSocketAddress(0); 
+				clientSocket.bind(local); 
+				InetSocketAddress remote = new InetSocketAddress(m.ipAdresse, m.port); 
+				clientSocket.connect(remote); 
+				clientSocket.write(b);
+				clientSocket.close();
+			} catch (Exception e) {
+				//Who cares ?
 			}
-		} catch (IOException e) {}
+		}
 	}
- }
+}
