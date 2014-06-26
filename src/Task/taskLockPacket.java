bbf93c45f513896b23116ac6b46dd714d78500da
Eltonio <antoine.sauvage@melix.net>
2014-06-26 17:13:02 +0200

    J'avais oublié un fichier...

---
+++ src/Task/taskLockPacket.java
@@ -0,0 +1,35 @@
+package Task;
+
+import java.net.Socket;
+import java.util.concurrent.Callable;
+
+public class taskLockPacket implements Callable
+{
+	Socket s;
+
+	/**
+	 * @author Antoine
+	 *
+	 * @param s : Socket sur lequel la communication se fait
+	 * @param asker : true si c'est celui qui a demandé le lock, false sinon.
+	 *	Ce reunnable est executé lorsqu'on reçoit l'instruction de locker des paquets sur le socket.
+	 */
+
+	public taskLockPacket(Socket socket){
+		s = socket;
+	}
+
+	public boolean run() {
+		//Etape 1 : renvoyer le message d'ACK
+		
+		String id;
+		//s.write().
+		//Etape 2 : attendre de recevoir l'identifiant du paquet qui demande le lock
+		
+		//Etape 3 : effectuer le lock si c'est possible.
+		
+		//Etpae 4 : répondre que le lock a bien été effectué
+
+	}
+
+}
