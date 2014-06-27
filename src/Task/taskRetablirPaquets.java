05074e87a0dfd94ee4327a9f7c33a4c564ae930a
Eltonio <antoine.sauvage@melix.net>
2014-06-27 16:09:18 +0200

    Bouh !

--- src/Task/taskRetablirPaquets.java
+++ src/Task/taskRetablirPaquets.java
@@ -15,13 +15,19 @@ public class taskRetablirPaquets implements Runnable {
 	Paquet frere;
 	int numeroMort;
 	SocketChannel[] clientSocket = new SocketChannel[Global.NOMBRESOUSPAQUETS];
-	ByteBuffer[] b;
+	ByteBuffer[] b = new ByteBuffer[Global.NOMBRESOUSPAQUETS];
+	ByteBuffer temp;
+	Paquet reconstruit;
+	
+	
+	
 	public taskRetablirPaquets (Paquet f, int num) {
 		frere = f;
 		numeroMort = num;
 		for(int i = 0; i<Global.NOMBRESOUSPAQUETS;i++)
 			b[i] = ByteBuffer.allocate((int) (Global.PAQUET_SIZE+3));
 		
+		Paquet reconstruit = new Paquet(frere.idMachine-frere.idInterne+numeroMort,frere.owner);
 	
 	}
 	
@@ -31,7 +37,6 @@ public class taskRetablirPaquets implements Runnable {
 		
 		for(int i =0;i<Global.NOMBRESOUSPAQUETS;i++)
 			if(i!=numeroMort){
-				
 				try {
 					InetSocketAddress local = new InetSocketAddress(0); 
 					clientSocket[i].bind(local); 
@@ -40,17 +45,43 @@ public class taskRetablirPaquets implements Runnable {
 					clientSocket[i].write(Utilitaires.stringToBuffer(Message.DEMANDE_PAQUET));
 					
 					//Etape 2 : attendre que le monsieur réponde qu'il veut bien nous envoyer le paquet
-					
+					//Il faut utiliser la super fonction de simon.
 					
 					//Etape 3 : Envoyer le numero du paquet
-				
-				
-					//ETpae 5 : remercier
+					clientSocket[i].write(Utilitaires.stringToBuffer(frere.otherHosts.get(i).toString()+"-"+(frere.idMachine-frere.idInterne+i)));
+					//Etage 4 : recevoir le paquet dans le buffer
+					
+					clientSocket[i].read(b[i],0,Global.PAQUET_SIZE);
+					
+					
+					//Etape 5 : remercier
+					clientSocket[i].write(Utilitaires.stringToBuffer(Message.OK));
+					
+					
+					
+					
 				}
 				catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} 
+				}
+				
+				for(int j = 0;j<Global.PAQUET_SIZE;j++)
+				{
+					temp.clear();
+					
+					
+					temp.flip();
+					try {
+						reconstruit.fichier.write(temp);
+					}
+					catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+				reconstruit.remettrePositionZero();
 			}
 		
 		//Etape 2 : reconstruire le paquet
