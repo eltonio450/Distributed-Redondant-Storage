c4a52bd33f014a437ba50b396ee2b57fe937091c
Simon Matet <simon.matet@polytechnique.edu>
2014-06-27 09:48:56 +0200

    task pour traiter un mort (reprend le contenu de données)

--- src/Task/taskReactToDeath.java
+++ src/Task/taskReactToDeath.java
@@ -1,5 +1,15 @@
 package Task;
 
-public class taskReactToDeath {
-
+public class taskReactToDeath implements Runnable {
+	String ip;
+	int port;
+	
+	public taskReactToDeath (String ip, int port) {
+		this.ip = ip;
+		this.port = port;
+	}
+	
+	public void run () {
+		Stockage.Donnees.traiteUnMort(new Stockage.Machine(ip, port));
+	}
 }
