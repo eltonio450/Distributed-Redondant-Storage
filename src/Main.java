eb46a7fa38c4dea155aa4080ab0242be4a89c1f5
Eltonio <antoine.sauvage@melix.net>
2014-06-27 18:41:09 +0200

    Main... J'ai pas trop compris

--- src/Main.java
+++ src/Main.java
@@ -44,67 +44,12 @@ public class Main {
 		System.out.println("Ok");
 */
 
-
-<<<<<<< HEAD
-=======
-
-
-		Thread thread2 = new Thread( 
+		/*Thread thread2 = new Thread( 
 				new ThreadTcpServer(port,tailleBuffer),"TcpServer"); 
-		thread2.start();
+		thread2.start();*/
 
 
 		System.out.println("Serveurs lanc�s") ;
 		new Thread(new ThreadClient()).start();
-=======
-    System.out.println("Modal launched !");
-    System.out.print("Parsing arguments...............");
-    IniServer.iniServer(args);
-    System.out.println("Arguments parsed...");
-    System.out.println("Ok");
->>>>>>> branch 'master' of https://github.com/eltonio450/modal.git
-
-    System.out.print("Initializing data...............");
-    IniDonnees.iniDonnees();
-    System.out.println("Data initialized...");
-    System.out.println("Ok");
-    
-    System.out.print("Enslaving innocent threads......");
-    Slaver.initialize();
-    System.out.println("Ok");
-    System.out.println("All set and ready to go !");
-
-    System.out.print("Starting TCP Server.............");
-    IniTCP.iniTCP();
-    System.out.println("Ok");
-
-    System.out.print("Getting server list.............");
-    ServerGetter.getServerList();
-    System.out.println("Ok");
-
-    System.out.print("Starting UDP server.............");
-    IniUDP.iniUDP();
-    System.out.println("Ok");
-    
-	System.out.println("All set and ready to go !");
-
-		
-		/*
->>>>>>> 9c7c8f3 Bladiboulga
->>>>>>> branch 'master' of https://github.com/eltonio450/modal.git
-		int port = 5656 ;
-		int tailleBuffer = 100 ;
-
-
-
-		Thread thread2 = new Thread( 
-				new ThreadTcpServer(port,tailleBuffer),"TcpServer"); 
-		thread2.start();
-
-
-		System.out.println("Serveurs lanc�s") ;
-		new Thread(new ThreadClient()).start();
-	
 	}
-
 }
