85b59139b747e0f06fdae854dced110f5f3e1249
Simon Matet <simon.matet@polytechnique.edu>
2014-06-27 15:29:23 +0200

    Bwahahaha

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,5 @@
-<<<<<<< HEAD
-import StartUpRoutine.IniServer;
 import StartUpRoutine.IniDonnees;
-import StartUpRoutine.IniTCP;
-import StartUpRoutine.IniUDP;
-import StartUpRoutine.ServerGetter;
-
-
-public class Main {
-
-
-  public static void main(String[] args)  {
-    System.out.println("Modal launched !");
-    System.out.print("Parsing arguments...............");
-    IniServer.iniServer(args);
-    System.out.println("Arguments parsed...");
-    System.out.println("Ok");
-    
-    System.out.print("Initializing data...............");
-    IniDonnees.iniDonnees();
-    System.out.println("Data initialized...");
-    System.out.println("Ok");
-    
-    System.out.print("Starting TCP Server.............");
-    IniTCP.iniTCP();
-    System.out.println("Ok");
-    
-    System.out.print("Getting server list.............");
-    ServerGetter.getServerList();
-    System.out.println("Ok");
-    
-    System.out.print("Starting UDP server.............");
-    IniUDP.iniUDP();
-    System.out.println("Ok");
-    System.out.println("All set and ready to go !");
-  }
-}
-=======
 import StartUpRoutine.IniServer;
-import StartUpRoutine.IniDonnees;
 import StartUpRoutine.IniTCP;
 import StartUpRoutine.IniUDP;
 import StartUpRoutine.ServerGetter;
@@ -47,6 +9,7 @@ public class Main {
 
 
 	public static void main(String[] args)  {
+		String fuck = "fuck la de";
 		System.out.println("Modal launched !");
 		System.out.print("Parsing arguments...............");
 		IniServer.iniServer(args);
@@ -72,4 +35,3 @@ public class Main {
 		System.out.println("All set and ready to go !");
 	}
 }
->>>>>>> branch 'master' of https://github.com/eltonio450/modal.git
